d19356cc6e4b481411e989eb368fdc6bc02bf81c
jiabo <843051627@qq.com>
2026-08-25 11:38:35 +0800

    feat: update mangrove-flux logic

--- internal/service/cache.go
+++ internal/service/cache.go
@@ -16,8 +16,8 @@ func NewReadingCache() *ReadingCache {
 	return &ReadingCache{byDeployment: map[string][]model.FluxReading{}}
 }
 func (c *ReadingCache) Update(reading model.FluxReading) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.byDeployment[reading.DeploymentID] = append(c.byDeployment[reading.DeploymentID], reading)
 }
 func (c *ReadingCache) All(deploymentID string) []model.FluxReading {
